572ade5998a842928cd54935e3582713b9119046
HanSuYeon <js7122420@gmail.com>
2024-09-19 11:31:47 +0900

    fix test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#해당 코드 실행시켰을 때 테스트가 마킹되어 있는 함수일 경우 테스트 함수로 분류되고
-#assert 키워드 내에 있는 이 조건문이 참이라면 테스트가 통과되고 실패라면 fail 뜨는 코드
+#pip3 install pytest
+#pytest test.py
 import pytest
 
 def func(x):
@@ -7,4 +7,4 @@ def func(x):
 
 #테스트 함수
 def test_answer():
-  assert func(3) == 5
+  assert func(4) == 5
